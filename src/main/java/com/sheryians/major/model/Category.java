56cc9a541913e61f950b10fa2b48324e9a4be31d
Vihanga Sakwithi <92682884+VihangaSakwithi@users.noreply.github.com>
2022-12-07 06:32:12 +0530

    Add files via upload

---
+++ src/main/java/com/sheryians/major/model/Category.java
@@ -0,0 +1,15 @@
+package com.sheryians.major.model;
+
+import javax.persistence.*;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private int id;
+    private String name;
+}
